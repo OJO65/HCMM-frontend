a45fd8278e013e9874df690506ac67bf201740c8
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2026-02-13 23:42:40 +0300

    feat: cook dashboard placeholder

---
+++ src/app/features/cook/cook-dashboard/cook-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cook-dashboard',
+  imports: [],
+  templateUrl: './cook-dashboard.component.html',
+  styleUrl: './cook-dashboard.component.scss',
+})
+export class CookDashboardComponent {
+
+}
