2a84fc121ea545820c21096576d782de25a2b50f
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2026-02-13 23:43:06 +0300

    feat: browse meals placeholder

---
+++ src/app/features/customer/browse-meals/browse-meals.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-meals',
+  imports: [],
+  templateUrl: './browse-meals.component.html',
+  styleUrl: './browse-meals.component.scss',
+})
+export class BrowseMealsComponent {
+
+}
