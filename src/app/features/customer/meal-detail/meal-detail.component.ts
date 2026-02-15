4a19f8cc6757aa740dd3d3ce091417151dccaf70
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2026-02-15 20:52:06 +0300

    feat: meal-detail placeholder

---
+++ src/app/features/customer/meal-detail/meal-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-detail',
+  imports: [],
+  templateUrl: './meal-detail.component.html',
+  styleUrl: './meal-detail.component.scss',
+})
+export class MealDetailComponent {
+
+}
