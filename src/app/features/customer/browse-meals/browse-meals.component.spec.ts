ec89027b7bc6adc7d3c3d0c2fbf3eb373b249e95
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2026-02-19 15:08:18 +0300

    chore: browse-meals unit test placeholder

---
+++ src/app/features/customer/browse-meals/browse-meals.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BrowseMealsComponent } from './browse-meals.component';
+
+describe('BrowseMealsComponent', () => {
+  let component: BrowseMealsComponent;
+  let fixture: ComponentFixture<BrowseMealsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BrowseMealsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BrowseMealsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
