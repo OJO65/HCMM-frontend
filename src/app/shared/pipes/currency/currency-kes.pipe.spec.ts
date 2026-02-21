9c1399d6290a7c1fc3efc3156e95ed908e649117
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2026-02-21 20:32:10 +0300

    chore: currency pipe unit test template

---
+++ src/app/shared/pipes/currency/currency-kes.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CurrencyKesPipe } from './currency-kes.pipe';
+
+describe('Pipe: CurrencyKese', () => {
+  it('create an instance', () => {
+    let pipe = new CurrencyKesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
